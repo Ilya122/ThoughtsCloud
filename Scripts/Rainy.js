67d337a7ae4507e6161ca00d6017672af80803b7
Ilya <Sorcerer122@gmail.com>
2022-04-20 21:28:06 +0300

    Fix umbrella rain drop collisions

--- Scripts/Rainy.js
+++ Scripts/Rainy.js
@@ -1,15 +1,43 @@
 class RainDrop extends DrawableEntity {
-    constructor(image, x, y, velocityX, velocityY) {
+    constructor(image, x, y, velocityX, velocityY, gravity) {
         super(image, x, y, velocityX, velocityY);
+        this.Circle = { Radius: image.width / 2 };
+        this.Gravity = gravity;
     }
 
     Update(context, canvas) {
-        super.Update(context, canvas);
+        this.Position.X += this.Velocity.X;
+        this.Position.Y += this.Velocity.Y;
+
+        if (this.Gravity > 0) {
+            this.Velocity.Y += this.Gravity;
+        }
+        // let velXDec = 0.005;
+        // if (this.Velocity.X > 0) {
+        //     this.Velocity.X -= velXDec;
+        //     if (this.Velocity.X < velXDec) {
+        //         this.Velocity.X = 0;
+        //     }
+        // } else if (this.Velocity.X < 0) {
+        //     this.Velocity.X += velXDec;
+        //     if (this.Velocity.X > -velXDec) {
+        //         this.Velocity.X = 0;
+        //     }
+        // }
 
         // Check for umbrella pos and bounce
         if (this.X > canvas.width) {
             this.X = 0;
         }
+    }
+
+    Draw(context, canvas) {
+        super.Draw(context, canvas);
+
+        // let halfSize = this.Image.width / 2;
+        // context.beginPath();
+        // context.arc(this.Position.X + halfSize, this.Position.Y + halfSize, this.Circle.Radius, 0, 2 * Math.PI);
+        // context.fill();
     }
 
 }
@@ -21,12 +49,11 @@ class Umbrealla {
 
         this.Image = img;
         this.Position = { X: 0, Y: 0 };
-
+        this.Circle = { Radius: 64 };
         this.IsRegistered = false;
     }
 
     Update(context, canvas) {
-
         if (!this.IsRegistered) {
             let t = this;
             canvas.addEventListener("mousemove", function(e) {
@@ -41,16 +68,20 @@ class Umbrealla {
     }
     Draw(context, canvas) {
         context.drawImage(this.Image, this.Position.X, this.Position.Y);
+
+        // context.beginPath();
+        // context.arc(this.Position.X + 64, this.Position.Y + 64, this.Circle.Radius, 0, 2 * Math.PI);
+        // context.fill();
     }
 
-    GetMousePos(canvas, evt) {
-        var rect = canvas.getBoundingClientRect();
-        return {
-            X: evt.clientX - rect.left,
-            Y: evt.clientY - rect.top
-        };
+    Collide(rainDrop) {
+        var dx = rainDrop.Position.X - (rainDrop.Image.width / 2) - this.Position.X - (this.Image.width / 2);
+        var dy = rainDrop.Position.Y - (rainDrop.Image.height / 2) - this.Position.Y - (this.Image.height / 2);
+        var rSum = rainDrop.Circle.Radius + this.Circle.Radius;
+        return (dx * dx + dy * dy <= rSum * rSum);
     }
 }
+
 class Rainy extends Weather {
     constructor(canvas) {
         super();
@@ -59,21 +90,38 @@ class Rainy extends Weather {
 
         this.Umbrealla = new Umbrealla();
         this.RainDrops = [];
-        this.NumberOfDrops = 25;
+        this.NumberOfDrops = 100;
     }
 
     Update(context, canvas) {
+        let umbrella = this.Umbrealla;
+
         this.RainDrops.map(rainDrop => {
             if (rainDrop.Position.Y > canvas.height) {
                 let randX = getRand(0, canvas.width - 10);
                 rainDrop.Position.X = randX;
                 rainDrop.Position.Y = -20;
-                rainDrop.Velocity.Y = 5;
+                rainDrop.Velocity.X = 0;
+                rainDrop.Velocity.Y = 6;
 
             }
 
-            rainDrop.Velocity.Y += 0.5;
+            rainDrop.Velocity.Y += 0.3;
             rainDrop.Update(context, canvas);
+
+            if (umbrella.Collide(rainDrop)) {
+                let newXVel = 0;
+                let newVel = -10;
+                if (rainDrop.Position.X + (rainDrop.Image.width / 2) > (umbrella.Position.X + (umbrella.Image.width / 2))) {
+                    newXVel = 20;
+                } else {
+                    newXVel = -20;
+                }
+                rainDrop.Velocity.X = newXVel;
+                rainDrop.Velocity.Y = newVel;
+            } else {
+                //rainDrop.Velocity.X = 0;
+            }
         });
 
         if (this.RainDrops.length < this.NumberOfDrops) {
@@ -99,7 +147,7 @@ class Rainy extends Weather {
         img.src = "rainDrop.png";
         let randX = getRand(0, canvas.width - 10);
 
-        this.RainDrops.push(new RainDrop(img, randX, 0, 0, 5));
+        this.RainDrops.push(new RainDrop(img, randX, 0, 0, 3, 0.2));
     }
 
 }
